733442443c61f9aefeae83bdafb486d4e69eecb8
Vtube <gledaimecom@gmail.com>
2026-06-27 23:34:15 +0300

    Update Embed.ts

--- constants/Embed.ts
+++ constants/Embed.ts
@@ -2,8 +2,8 @@
 
 export const defaultSandboxAllowedForMovie = true;
 export const defaultSandboxAllowedForTv = true;
-export const defaultMovieUrlTemplate = "https://player.videasy.net/movie/{TMDBID}";
-export const defaultTvShowUrlTemplate = "https://player.videasy.net/tv/{TMDBID}/{SEASON}/{EPISODE}";
+export const defaultMovieUrlTemplate = "https://stream.vidplay.to/watch/movie/{TMDBID}";
+export const defaultTvShowUrlTemplate = "https://stream.vidplay.to/watch/tv/{TMDBID}?s={SEASON}&e={EPISODE}";
 
 // export const movieUrlTemplate = "https://vidsrc.to/embed/movie/{IMDBID}";
 // export const seriesUrlTemplate = "https://vidsrc.to/embed/tv/{IMDBID}/{SEASON}/{EPISODE}";
